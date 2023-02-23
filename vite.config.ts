84194569e6f1e312ad58e4756d3d0db4f33c9f72
Rob Gordon <rchristopher.gordon@gmail.com>
2023-02-23 11:05:09 -0500

    add necessary pwa content

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,36 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.png"],
       registerType: "autoUpdate",
       devOptions: {
         enabled: true,
+      },
+      manifest: {
+        name: "Too Too",
+        short_name: "TooToo",
+        description: "Local-First to-do application",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
       },
     }),
   ],
